4d0c7370b810698ac89bced540d2e6ce6aa02a5e
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 19:43:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+var pbj = {
+    bread: "white",
+    ingredients: ["peanut butter", "jelly"],
+    cut: "triangles"
+}
+
+function Sandwich(bread, ingredients, cut) {
+    this.bread = bread
+    this.ingredients = ingredients
+    this.cut = cut
+}
